72a0ba1e887566c27e8d0ac946b855811aab0bfa
CTepedino <ctepedino@itba.edu.ar>
2024-09-21 02:23:44 -0300

    changed doctors collection from set to map

--- server/src/main/java/ar/edu/itba/pod/server/repository/DoctorRepository.java
+++ server/src/main/java/ar/edu/itba/pod/server/repository/DoctorRepository.java
@@ -2,22 +2,27 @@ package ar.edu.itba.pod.server.repository;
 
 import ar.edu.itba.pod.server.model.Doctor;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DoctorRepository {
 
-    private final Set<Doctor> doctors;
+    private final Map<String, Doctor> doctors;
 
     public DoctorRepository(){
-        doctors = new HashSet<>();
+        doctors = new HashMap<>();
     }
 
     public void addDoctor(String name, int maxLevel) throws IllegalArgumentException{
 
         Doctor doctor = new Doctor(name, maxLevel);
-        if (!doctors.add(doctor)){
+        if(doctors.putIfAbsent(name, doctor) == null) {
             throw new IllegalArgumentException("Doctor " + name + " already exists");
         }
     }
+
+    public Doctor getDoctor(String name){
+        return doctors.get(name);
+    }
+
 }
