b5a8f049de791fd931166f2a3bb2af2adfbfbb2d
LucaBloise <lbloise@itba.edu.ar>
2024-09-24 02:24:17 -0300

    queryClient

---
+++ client/src/main/java/ar/edu/itba/pod/client/queryClient.java
@@ -0,0 +1,108 @@
+package ar.edu.itba.pod.client;
+
+import ar.edu.itba.pod.grpc.query.*;
+import emergencyRoom.Messages;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class queryClient {
+    private static QueryServiceGrpc.QueryServiceBlockingStub blockingStub;
+    private static Path outPath;
+
+    public static void main(String[] args){
+        String address = System.getProperty("serverAddress");
+        if (address == null){
+            System.out.println("No address specified");
+            return;
+        }
+
+        String action = System.getProperty("action");
+        if (action == null){
+            System.out.println("No action specified");
+            return;
+        }
+
+        String pathString = System.getProperty("outPath");
+        if (outPath == null){
+            System.out.println("No output path specified");
+            return;
+        }
+        outPath = Paths.get(pathString);
+
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(address).usePlaintext().build();
+        blockingStub = QueryServiceGrpc.newBlockingStub(channel);
+
+        switch(action){
+            case "queryRooms":
+                queryRooms();
+                break;
+            case "queryWaitingRoom":
+                queryWaitingRoom();
+                break;
+            case "queryCares":
+                queryCares();
+                break;
+            default:
+                System.out.println("Unrecognized action: " + action);
+        }
+    }
+
+    private static void queryRooms(){
+        try {
+            QueryRoomsResponse response = blockingStub.queryRooms(com.google.protobuf.Empty.newBuilder().build());
+            List<String> csvData = new ArrayList<>();
+            csvData.add("Room,Status,Patient,Doctor");
+            for(RoomInfo room : response.getRoomsList()){
+                if(room.getStatus() == RoomStatus.ROOM_STATUS_FREE){
+                    csvData.add(String.format("%d,Free,,", room.getRoom()));
+                } else if (room.getStatus() == RoomStatus.ROOM_STATUS_OCCUPIED) {
+                    csvData.add(String.format("%d,Occupied,%s (%d),%s (%d)", room.getRoom(), room.getPatient().getName(), room.getPatient().getLevel(), room.getDoctor().getName(), room.getDoctor().getMaxLevel()));
+                }
+            }
+            Files.write(outPath, csvData);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void queryWaitingRoom(){
+        try {
+            QueryWaitingRoomResponse response = blockingStub.queryWaitingRoom(com.google.protobuf.Empty.newBuilder().build());
+            List<String> csvData = new ArrayList<>();
+            csvData.add("Patient,Level");
+            for(Messages.PatientInfo patient : response.getPatientsList()){
+                csvData.add(String.format("%s,%d", patient.getName(), patient.getLevel()));
+            }
+            Files.write(outPath, csvData);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void queryCares(){
+        String room = System.getProperty("room");
+        QueryCaresRequest request;
+        if(room == null){
+            request = QueryCaresRequest.newBuilder().build();
+        } else {
+            request = QueryCaresRequest.newBuilder().setRoom(Integer.parseInt(room)).build();
+        }
+        try {
+            QueryCaresResponse response = blockingStub.queryCares(request);
+            List<String> csvData = new ArrayList<>();
+            csvData.add("Room,Patient,Doctor");
+            for(CaredInfo row : response.getCaresList()){
+                csvData.add(String.format("%d,%s (%d),%s (%d)", row.getRoom(), row.getPatient().getName(), row.getPatient().getLevel(), row.getDoctor().getName(), row.getDoctor().getMaxLevel()));
+            }
+            Files.write(outPath, csvData);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
