22f91f2bedd659f806fc4a48c0cd11a46c712909
juanamendonca <108553386+juanamendonca@users.noreply.github.com>
2024-09-26 17:26:58 -0300

    Update EmergencyCareServiceTest.java

--- server/src/test/java/ar/edu/itba/pod/server/repository/EmergencyCareServiceTest.java
+++ server/src/test/java/ar/edu/itba/pod/server/repository/EmergencyCareServiceTest.java
@@ -3,6 +3,7 @@ package ar.edu.itba.pod.server.repository;
 import ar.edu.itba.pod.server.service.EmergencyCareService;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
+import emergencyRoom.Messages;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -43,11 +44,11 @@ public class EmergencyCareServiceTest {
     }
 
     @Test
-    public final void PatientsAhead() throws InterruptedException, ExecutionException {
+    public final void carePatient() throws InterruptedException, ExecutionException {
         waitingRoomRepository.addPatient("pablo", 4);
         waitingRoomRepository.addPatient("juana", 5);
-        waitingRoomRepository.addPatient("luca", 4);
-        waitingRoomRepository.addPatient("cristian", 4);
+        waitingRoomRepository.addPatient("lucas", 4);
+        waitingRoomRepository.addPatient("cris", 4);
         waitingRoomRepository.addPatient("paula", 3);
         waitingRoomRepository.addPatient("manuel", 2);
         waitingRoomRepository.addPatient("mora", 5);
@@ -57,12 +58,67 @@ public class EmergencyCareServiceTest {
 
 
         doctorRepository.addDoctor("doctor1", 5);
+        doctorRepository.setDoctorStatus("doctor1", Messages.DoctorStatus.DOCTOR_STATUS_AVAILABLE);
         doctorRepository.addDoctor("doctor2", 2);
+        doctorRepository.setDoctorStatus("doctor2", Messages.DoctorStatus.DOCTOR_STATUS_AVAILABLE);
         doctorRepository.addDoctor("doctor3", 3);
 
         roomRepository.addRoom();
         roomRepository.addRoom();
 
+        waitingRoomRepository.startCare(1);
+
+        assertEquals(0, waitingRoomRepository.getPatientsAhead(waitingRoomRepository.findByName("mora")));
+
+        waitingRoomRepository.startCare(2);
+
+        assertEquals(0, waitingRoomRepository.getPatientsAhead(waitingRoomRepository.findByName("mora")));
+        assertEquals(6, waitingRoomRepository.getPatientsAhead(waitingRoomRepository.findByName("carlos")));
+
+        waitingRoomRepository.endCare(1, "juana", "doctor1");
+
+        waitingRoomRepository.startCare(1);
+
+        assertEquals(0, waitingRoomRepository.getPatientsAhead(waitingRoomRepository.findByName("pablo")));
+
+    }
+
+    @Test
+    public final void careAllPatients() throws InterruptedException, ExecutionException {
+        waitingRoomRepository.addPatient("pablo", 4);
+        waitingRoomRepository.addPatient("juana", 5);
+        waitingRoomRepository.addPatient("lucas", 4);
+        waitingRoomRepository.addPatient("cris", 4);
+        waitingRoomRepository.addPatient("paula", 3);
+        waitingRoomRepository.addPatient("manuel", 2);
+        waitingRoomRepository.addPatient("mora", 5);
+        waitingRoomRepository.addPatient("maria", 1);
+        waitingRoomRepository.addPatient("carlos", 2);
+        waitingRoomRepository.addPatient("mia", 4);
+
+
+        doctorRepository.addDoctor("doctor1", 5);
+        doctorRepository.setDoctorStatus("doctor1", Messages.DoctorStatus.DOCTOR_STATUS_AVAILABLE);
+        doctorRepository.addDoctor("doctor2", 2);
+        doctorRepository.setDoctorStatus("doctor2", Messages.DoctorStatus.DOCTOR_STATUS_AVAILABLE);
+        doctorRepository.addDoctor("doctor3", 3);
+
+        roomRepository.addRoom();
+        roomRepository.addRoom();
+
+        waitingRoomRepository.startAllCare();
+
+        assertEquals(0, waitingRoomRepository.getPatientsAhead(waitingRoomRepository.findByName("mora")));
+
+        waitingRoomRepository.startCare(2);
+
+        waitingRoomRepository.endCare(1, "juana", "doctor1");
+
+        waitingRoomRepository.startAllCare();
+
+        waitingRoomRepository.endCare(1, "mora", "doctor1");
+
+        waitingRoomRepository.endCare(2, "manuel", "doctor2");
     }
 
 
